79b98423b0915500d99ca9f0b98f17bab88488c5
Nicolas Boisvert <nicklay@me.com>
2020-10-03 16:05:23 -0400

    users-server - Add users base test

---
+++ test/boreale/users_test.exs
@@ -0,0 +1,38 @@
+defmodule Boreale.DomainsTest do
+  use Boreale.TestCase
+
+  alias Boreale.{Domains, Storage}
+
+  describe "start_link/1" do
+    setup do
+      _ = Domains.start_link([])
+      :ok
+    end
+  end
+
+  @domain "public.com"
+  describe "public?/1" do
+    test "only public domains are public" do
+      with_mock(Storage, read_dets: fn _, _ -> [[@domain, DateTime.utc_now()]] end) do
+        Domains.sync()
+        assert Domains.public?(@domain)
+        refute Domains.public?("another.domain.com")
+      end
+    end
+  end
+
+  describe "sync/0" do
+    test "updates itself on sync" do
+      with_mock(Storage, read_dets: fn _, _ -> [] end) do
+        Domains.sync()
+        refute Domains.public?(@domain)
+      end
+
+      with_mock(Storage, read_dets: fn _, _ -> [[@domain, DateTime.utc_now()]] end) do
+        Domains.sync()
+        assert Domains.public?(@domain)
+        refute Domains.public?("another.domain.com")
+      end
+    end
+  end
+end
