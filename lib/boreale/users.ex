84df2770a2bf860aa2a37021c9bc07200116d9a0
Nicolas Boisvert <nicklay@me.com>
2020-10-03 16:51:57 -0400

    users-server -

--- lib/boreale/users.ex
+++ lib/boreale/users.ex
@@ -1,5 +1,4 @@
 defmodule Boreale.Users do
-<<<<<<< HEAD
   use GenServer
 
   alias Boreale.Storage
@@ -66,30 +65,10 @@ defmodule Boreale.Users do
   defp read_from_dets(%State{file: file}) do
     file
     |> Storage.read_dets({:"$1", :"$2", :"$3"})
-=======
-  @users_file "data/users.dets"
-
-  defp all do
-    {:ok, table} =
-      File.cwd!()
-      |> Path.join(@users_file)
-      |> String.to_atom()
-      |> :dets.open_file(type: :set)
-
-    :dets.match(table, {:"$1", :"$2", :"$3"})
->>>>>>> e4d5c07a79d3f18351630b047570d360e539c2cc
     |> Enum.map(fn x -> List.to_tuple(x) end)
     |> Enum.map(fn {u, pw, _} -> {u, pw} end)
     |> Enum.into(%{})
   end
 
-<<<<<<< HEAD
   defp log(:info, message), do: Logger.info("[#{inspect(__MODULE__)}] #{message}")
-=======
-  def valid?(user, password) do
-    password_hash = Map.get(all(), user, nil)
-
-    not is_nil(password_hash) and Bcrypt.verify_pass(password, password_hash)
-  end
->>>>>>> e4d5c07a79d3f18351630b047570d360e539c2cc
 end
