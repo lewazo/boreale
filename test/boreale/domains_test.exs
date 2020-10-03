24081b8f29dda9f9c2f7d22889fbfba3024fa54e
Nicolas Boisvert <nicklay@me.com>
2020-10-03 16:04:13 -0400

    domains-server - Split tests

--- test/boreale/domains_test.exs
+++ test/boreale/domains_test.exs
@@ -8,9 +8,21 @@ defmodule Boreale.DomainsTest do
       _ = Domains.start_link([])
       :ok
     end
+  end
 
-    @domain "public.com"
-    test "syncs with dets content" do
+  @domain "public.com"
+  describe "public?/1" do
+    test "only public domains are public" do
+      with_mock(Storage, read_dets: fn _, _ -> [[@domain, DateTime.utc_now()]] end) do
+        Domains.sync()
+        assert Domains.public?(@domain)
+        refute Domains.public?("another.domain.com")
+      end
+    end
+  end
+
+  describe "sync/0" do
+    test "updates itself on sync" do
       with_mock(Storage, read_dets: fn _, _ -> [] end) do
         Domains.sync()
         refute Domains.public?(@domain)
